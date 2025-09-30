e07bd5a074bdf01eee4bc981b5de2aac0e3dfe3b
Kezaldius <0kezald0@gmail.com>
2025-09-30 19:14:31 +0300

    Implemented global exception handler

---
+++ src/main/java/com/cosmiccats/intergalactic_market/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.cosmiccats.intergalactic_market.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(
+            MethodArgumentNotValidException ex, HttpServletRequest request) {
+
+        FieldError fieldError = ex.getBindingResult().getFieldError();
+        String errorMessage = "Validation failed for object '" + fieldError.getObjectName() + 
+                              "'. Field '" + fieldError.getField() + "': " + fieldError.getDefaultMessage();
+        
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", "Bad Request");
+        body.put("message", errorMessage);
+        body.put("path", request.getRequestURI());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+}
